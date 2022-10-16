8f2fe3947964315dbf38bea044f777d5ac87815c
erynder-z <stfn.bgr@gmail.com>
2022-10-16 21:03:15 +0200

    Render messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const format = require('date-fns/format');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+     {
+          text: 'Hi there!',
+          user: 'Amando',
+          added: new Date(),
+     },
+     {
+          text: 'Hello World!',
+          user: 'Charles',
+          added: new Date(),
+     },
+];
+
+/* GET board index */
+router.get('/', function (req, res, next) {
+     res.render('index', {
+          title: 'Mini Message Board',
+          messages: messages,
+          format: format,
+     });
 });
 
 module.exports = router;
