ff87a1306345eebc812a7ee0f69cdf95f6a84b25
erynder-z <stfn.bgr@gmail.com>
2022-10-17 14:50:06 +0200

    Add some basic styling

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,29 @@ const express = require('express');
 const format = require('date-fns/format');
 const router = express.Router();
 
+const colors = [
+     '#b58900',
+     '#cb4b16',
+     '#dc322f',
+     '#d33682',
+     '#6c71c4',
+     '#268bd2',
+     '#2aa198',
+     '#859900',
+];
+
 const messages = [
      {
-          text: 'Hi there!',
+          text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, in!',
           user: 'Amando',
           added: new Date(),
+          color: colors[Math.floor(Math.random() * colors.length)],
      },
      {
           text: 'Hello World!',
           user: 'Charles',
           added: new Date(),
+          color: colors[Math.floor(Math.random() * colors.length)],
      },
 ];
 
@@ -35,6 +48,7 @@ router.post('/new', function (req, res, next) {
           text: req.body.message,
           user: req.body.author,
           added: new Date(),
+          color: colors[Math.floor(Math.random() * colors.length)],
      });
      res.redirect('/');
 });
